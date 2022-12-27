86842c216a4a866c08cb06b9ebd9510b787dcf54
tomavic <hbasheer@live.com>
2022-12-27 19:11:47 +0200

    test: 🧪 bump test coverage

--- src/app/context/books-context.spec.tsx
+++ src/app/context/books-context.spec.tsx
@@ -1,15 +1,28 @@
-import { act, renderHook } from '@testing-library/react';
+import { act, render, renderHook } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import { describe, expect, it } from 'vitest';
 import { useBooksContext } from './books-context';
 
+import App from '../app';
+
 describe('useBooksContext hook', async () => {
-  it('should get my books', async () => {
+  it('should init store with default values', async () => {
     const { result } = renderHook(() => useBooksContext());
+
     act(() => {
       result.current.state.books = [];
+      result.current.state.searchResults = [];
+      result.current.state.loading = false;
     });
     expect(result.current.state.books).toEqual([]);
     expect(result.current.dispatch).toBeInstanceOf(Function);
+    expect(result.current.state.searchResults).toEqual([]);
+  });
+
+  it('should render app component', () => {
+    const { baseElement } = render(<App />, { wrapper: BrowserRouter });
+
+    expect(baseElement).toBeTruthy();
   });
 });
