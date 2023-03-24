f822692b84642332008f73954c95d0d7bdaf2a82
tomavic <hbasheer@live.com>
2023-03-24 21:26:29 +0200

    chore: :hammer: update

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,6 @@
 import { useRoutes } from 'react-router-dom';
 import routes from './app.routes';
 
-/**
- * App Component
- *
- * Contain direct definition to the Application Component Graph
- * @returns React.ReactElement<any, string | React.JSXElementConstructor<any>> | null
- */
 export function App() {
   const content = useRoutes(routes);
   return content;
